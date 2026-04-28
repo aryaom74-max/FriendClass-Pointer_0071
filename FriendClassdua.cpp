f13cafab41f883b69662c4b068c3f82ddeec675d
Muh Arya Saputra <aryaom74@gmail.com>
2026-04-28 16:01:05 +0700

    Implement setNama function in FriendClassdua.cpp
    Added implementation of setNama function to set the name of mahasiswa.

--- FriendClassdua.cpp
+++ FriendClassdua.cpp
@@ -9,3 +9,15 @@ public:
     friend void setNama(mahasiswa &a, string);
 };
 
+void setNama(mahasiswa &a, string b)
+{ 
+    a.Nama = b;
+    cout << a.Nama;
+}
+
+int main()
+{
+    mahasiswa joko;
+    setNama(joko, "kairi Kumar");
+    return 0;
+}
