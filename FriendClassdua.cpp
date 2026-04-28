560c0912d81d13d9f5f47d9de171d1044f846511
Muh Arya Saputra <aryaom74@gmail.com>
2026-04-28 16:00:51 +0700

    Update FriendClassdua.cpp

--- FriendClassdua.cpp
+++ FriendClassdua.cpp
@@ -9,15 +9,3 @@ public:
     friend void setNama(mahasiswa &a, string);
 };
 
-void setNama(mahasiswa &a, string b)
-{ 
-    a.Nama = b;
-    cout << a.Nama;
-}
-
-int main()
-{
-    mahasiswa joko;
-    setNama(joko, "kairi Kumar");
-    return 0;
-}
